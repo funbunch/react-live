28375326c3398cfc07b252ad242bf53151c23b28
shannan <sbunch@outlook.com>
2016-11-02 03:31:12 +0000

    added bootstrap

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,14 +3,18 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var browserHistory = ReactRouter.browserHistory;
+var IndexRoute = ReactRouter.IndexRoute;
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var PromptContainer = require("../containers/PromptContainer");
 
 var routes = (
   <Router history={browserHistory}>
     <Route path='/' component={Main}>
-      <Route path='/home' component={Home}></Route>
+    <Route path='playerOne' component={PromptContainer}/>
+    <Route path='playerTwo/:playerOne' component={PromptContainer} />
+      <IndexRoute component = {Home} />
     </Route>
   </Router>
 );
